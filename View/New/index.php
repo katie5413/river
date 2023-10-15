83e523fe9ccff3e3f315bfab014b4eb00e15f60d
13 <katie87827@gmail.com>
2023-10-15 21:21:49 +0800

    fix: add noindex to disabled search engine

--- View/New/index.php
+++ View/New/index.php
@@ -8,6 +8,7 @@ include "../../pdoInc.php";
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link rel="stylesheet" type="text/css" href="../../Common/index.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" type="text/css" href="../../Components/TimeTunnel/index.css?v=<?php echo time(); ?>">
